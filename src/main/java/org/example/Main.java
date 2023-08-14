e1986b973eac46dddb06dc32f87277f7fedef641
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-08-14 12:53:15 +0300

    innit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        NumberSquaredCalc numberSquaredCalc=new NumberSquaredCalc();
+        numberSquaredCalc.squaredNumber(18);
     }
 }
